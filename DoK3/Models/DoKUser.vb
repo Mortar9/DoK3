c19b21ba5a9cf0df6de001dc35744a238607427e
Martin M <altimartin17@hotmail.com>
2018-08-08 19:54:06 -0400

    VSCode

---
+++ DoK3/Models/DoKUser.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.EntityFrameworkCore
+Imports System.Collections.Generic
+
+Public Class DoKUser
+    Public Property UserName As String
+    Public Property ScreenName As String
+    Public Property DateCreated As DateTime
+End Class
+
+Public Class DoKUserContext Inherits DbContext
